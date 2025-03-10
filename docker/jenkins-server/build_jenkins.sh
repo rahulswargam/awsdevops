7cffe9741f4616bf2a9581260e9daa14eb932b2a
Rahul Swargam <rahulswargam@Rahuls-MacBook-Pro.local>
2025-03-10 18:20:41 +0530

    create docker build script

--- docker/jenkins-server/build_jenkins.sh
+++ docker/jenkins-server/build_jenkins.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Check if an image name is passed as an argument
-if [ -z "$1" ]; then
-  echo "No image name provided. Using default name: sr-jenkins"
+echo "Enter Name of the Image:"
+read IMAGE_NAME
+
+if [ -z "$IMAGE_NAME" ]; then
   IMAGE_NAME="sr-jenkins"
-else
-  IMAGE_NAME="$1"
+  echo "Default Image Name Taken: $IMAGE_NAME"
 fi
 
-# Set environment variables from credentials.env
-set -a
-source credentials.env
-set +a
-
-# Build the Docker image with the specified (or default) name
-docker build --build-arg JENKINS_USER=$JENKINS_USER --build-arg JENKINS_PASS=$JENKINS_PASS --build-arg JENKINS_FULL_NAME="$JENKINS_FULL_NAME" --build-arg JENKINS_EMAIL="$JENKINS_EMAIL" -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME .
