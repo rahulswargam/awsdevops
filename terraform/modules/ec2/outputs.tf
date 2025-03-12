5c0536d01e6a60ac0e55fe2ba401d84c299a73d8
Rahul Swargam <rahulswargam@Rahuls-MacBook-Pro.local>
2025-03-13 00:19:15 +0530

    updated outputs.tf

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,14 +1,13 @@
 # Outputs of the Instance
 
-output "instance_public_ip" {
-  value = aws_instance.devops_services.public_ip
-}
-
-# Output the security group ID and key pair name
 output "jenkins_security_group_id" {
   value = aws_security_group.devops_sg.id
 }
 
 output "key_pair_name" {
   value = data.aws_key_pair.devops_keypair.key_name
+}
+
+output "instance_public_ips" {
+  value = aws_instance.devops_services[*].public_ip
 }
