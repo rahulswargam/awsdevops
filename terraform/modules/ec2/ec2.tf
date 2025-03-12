59bd7cdebbf14e3ad7ef715fa801524fa2eafdb5
Rahul Swargam <rahulswargam@Rahuls-MacBook-Pro.local>
2025-03-13 00:18:55 +0530

    updated ec2.tf

--- terraform/modules/ec2/ec2.tf
+++ terraform/modules/ec2/ec2.tf
@@ -1,20 +1,17 @@
-
-
 # EC2 Instance Creation
-
 resource "aws_instance" "devops_services" {
-  ami                    = data.aws_ami.rhel_9.id
+
+  count                  = var.instance_count
+  ami                    = var.ami
   instance_type          = var.instance_type
-  key_name               = data.aws_key_pair.devops_keypair.key_name
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
   user_data              = file(var.user_data)
 
   tags = merge(
     var.tags,
     {
-      Name        = "${var.project_name}-instance"
-      Project     = var.project_name
-      Environment = var.environment
+      Name = var.instance_names[count.index]
     }
   )
 }
